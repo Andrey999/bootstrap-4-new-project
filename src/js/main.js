64043a42787bb45bab2dddaf2271b2266b8dc59d
Andrey999 <1985gromovandrey@gmail.com>
2019-04-03 17:15:15 +0300

    add block about and tabs

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,20 @@
 window.onload = function() {
+    var jsTriggers = document.querySelectorAll('.js-btn');
 
+    jsTriggers.forEach(function(trigger) {
+        trigger.addEventListener('click', function() {
+            var id = this.getAttribute('data-filter'),
+                content = document.querySelector('.js-tab-content[data-filter="'+id+'"]'),
+                activeTrigger = document.querySelector('.js-btn.active'),
+                activeContent = document.querySelector('.js-tab-content.active-content');
+
+            activeTrigger.classList.remove('active');
+            trigger.classList.add('active');
+
+            activeContent.classList.remove('active-content');
+            content.classList.add('active-content');
+        });
+    });
 };
+
 
